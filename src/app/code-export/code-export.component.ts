2fd6d716426cd068df9da2351e965faf7ca6fd0f
Matt <matt.leonard.22@gmail.com>
2017-12-22 13:40:32 -0800

    added some editor CSS to code that gets exported

--- src/app/code-export/code-export.component.ts
+++ src/app/code-export/code-export.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Inject } from '@angular/core';
+import { Component, OnInit, Input, Inject, OnChanges } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
@@ -15,7 +15,12 @@ export class CodeExportComponent implements OnInit {
               @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
+    
+  }
+
+  ngOnChanges() {
     this.exportedSurvey = this.data;
+    this.translateHTML();
   }
 
   closeDialog() {
@@ -23,11 +28,20 @@ export class CodeExportComponent implements OnInit {
   }
 
   translateHTML() {
-    let styles: string;
-    styles = "<style>form{background-color:" + this.data.settings.background +"; border:" + this.data.settings.border + 
+    console.log("translate ran");
+    let styles: string = "<style>form{background-color:" + this.data.settings.background +"; border:" + this.data.settings.border + 
              "; border-radius:" + this.data.settings.borderRadius  + "; font-family:" + this.data.settings.font + 
              "; font-size:" + this.data.settings.fontSize + "; padding:" + this.data.settings.padding + 
-             "; width:" + this.data.settings.width + "px;}</style>"
+             "; width:" + this.data.settings.width + "px;}.surveyQuestion{display:flex;align-items:center;justify-content" +
+             ":flex-start;min-height:30px;}.questionSide{margin-right:50px;display:flex;align-items:flex-start;}h1,h3,h5{margin:0;}" + 
+             ".checkboxLabel{margin-top:3px;}.checkboxOuter{display:flex;flex-direction:column;align-items:flex-start;}" + 
+             ".checkboxInner{display:flex;align-items:center;}.addressBlock{display:flex;flex-direction:column;}" + 
+             ".addressBlock input{padding:5px 0;margin-bottom:8px;}.gridQuestion{display:flex;flex-direction:column;}" + 
+             ".gridTable{border:1px solid #000000;}.gridTable td{border:1px solid #000000;border-collapse:collapse;padding:10px;}" + 
+             ".gridTableRow td{text-align:center;}.surveyQuestion label{padding-right:5px;}" + 
+             ".surveyQuestion input{background-color:transparent;height:20px;padding:0 5px;}" + 
+             ".surveyQuestion textarea{font-family:Arial, sans-serif;font-size:0.8em;padding:5px;background-color:transparent;}" + 
+             ".requiredStar{color:#ff0000;}</style>"
     console.log(styles);
   }
 
