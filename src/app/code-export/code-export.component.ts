45c2b43f4728221ebaa404f1ccf9651e5c5bca5c
Matt <matt.leonard.22@gmail.com>
2017-12-22 20:43:14 -0800

    text field translation looks good

--- src/app/code-export/code-export.component.ts
+++ src/app/code-export/code-export.component.ts
@@ -28,7 +28,7 @@ export class CodeExportComponent implements OnInit {
   }
 
   translateHTML() {
-    console.log("translate ran");
+    console.log(this.data.questions);
     let styles: string = "<style>form{background-color:" + this.data.settings.background +"; border:" + this.data.settings.border + 
              "; border-radius:" + this.data.settings.borderRadius  + "; font-family:" + this.data.settings.font + 
              "; font-size:" + this.data.settings.fontSize + "; padding:" + this.data.settings.padding + 
@@ -41,8 +41,90 @@ export class CodeExportComponent implements OnInit {
              ".gridTableRow td{text-align:center;}.surveyQuestion label{padding-right:5px;}" + 
              ".surveyQuestion input{background-color:transparent;height:20px;padding:0 5px;}" + 
              ".surveyQuestion textarea{font-family:Arial, sans-serif;font-size:0.8em;padding:5px;background-color:transparent;}" + 
-             ".requiredStar{color:#ff0000;}</style>"
-    console.log(styles);
+             ".requiredStar{color:#ff0000;}</style>";
+    let body: string = "<form action='#' method='post'>";
+    for (let i = 0; i < this.data.questions.length; i++) {
+      let options = this.data.questions[i].options;
+
+      switch (this.data.questions[i].type) {
+        case 'text':
+          let hide = options.hideQuestion;
+          let required = options.responseRequired;
+          console.log("text field: " + required);
+          body += "<div class='surveyQuestion' style='justify-content:" + options.align + ";'><div class='questionSide' style='padding:" + options.padding + ";'>";
+          // If hideQuestion is false, concat the label tag
+          if (!hide) {
+            body += "<label style='color:" + options.color + ";'>" + this.data.questions[i].label + "</label>";
+          }
+          // If required is true, concat the required star
+          if (required) {
+            body += "<span class='requiredStar'>*</span>"
+          }
+          body += "<input type='text' placeholder='" + options.placeholder + "' style='border:" + options.border + "; border-radius:" + options.borderRadius + ";'"
+          body += "</div></div>";
+        break;
+
+        case 'textarea':
+          hide = options.hideQuestion;
+          required = options.responseRequired;
+          console.log("text area: " + required);
+        break;
+
+        case 'email':
+          // email field HTML
+        break;
+
+        case 'address':
+          //address field HTML
+        break;
+
+        case 'phone':
+          // phone field HTML
+        break;
+
+        case 'select':
+          // select list HTML
+        break;
+
+        case 'radio':
+          // radio button HTML
+        break;
+
+        case 'checkbox':
+          // checkbox HTML
+        break;
+
+        case 'grid':
+          // grid HTML
+        break;
+
+        case 'heading':
+          // heading HTML
+        break;
+
+        case 'paragraph':
+          // paragraph HTML
+        break;
+
+        case 'image':
+          // image HTML
+        break;
+
+        case 'divider':
+          // divider HTML
+        break;
+
+        case 'spacer':
+          // spacer HTML
+        break;
+
+        case 'submit':
+          // submit button HTML
+        break;
+      }
+    }
+    body += "</form>";
+    console.log(body);
   }
 
 }
